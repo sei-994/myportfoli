7a6cf3e1c3ea25399a7d25b1c8fa35eb4683b62a
seiji <s.hoshino0517basket@gmail.com>
2020-12-15 16:07:13 +0900

    設定終了（eslint prettier),homevue作成

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-main>
+    <v-parallax
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-4">PORTFOLIO</h1>
+          <h4 class="subheading">my portfolio</h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scss></style>
