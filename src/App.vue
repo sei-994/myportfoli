ff0dbb9a88e684e718e00016c05ebe76a9105f62
seiji <s.hoshino0517basket@gmail.com>
2020-12-19 19:54:46 +0900

    スマホ版のヘッダーメニュー作成、common/constants.jsで共通化ソースコード省略化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
+  <v-app>
+    <!-- ここにヘッダーをコーディング -->
+    <header>
+      <v-app-bar app dark>
+        <!-- ここから追加 -->
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <!-- ここまで追加 -->
+        <v-toolbar-title>MyPortfolioSite</v-toolbar-title>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" fixed temporary>
+        <v-list nav dense>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title>
+                Home
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                About
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                Work
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                Service
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </header>
     <Home></Home>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -11,11 +46,13 @@ export default {
   name: "App",
 
   components: {
-    Home
+    Home,
   },
 
-  data: () => ({
-    //
-  })
+  data() {
+    return {
+      drawer: false,
+    }
+  },
 }
 </script>
---
+++ src/constants.js
@@ -0,0 +1,29 @@
+//共通化ソースコードの省略化
+export default {
+  menuItems: [
+    {
+      name: "HOME",
+      url: "#",
+    },
+    {
+      name: "ABOUT",
+      url: "#",
+    },
+    {
+      name: "WORK",
+      url: "#",
+    },
+    {
+      name: "SERVICE",
+      url: "#",
+    },
+    {
+      name: "BLOG",
+      url: "#",
+    },
+    {
+      name: "ACCESS",
+      url: "#",
+    },
+  ],
+}
